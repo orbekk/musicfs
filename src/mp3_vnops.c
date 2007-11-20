3e39e38e4de902dc4f570b690e7b5987bebcf2ee
lulf@carrot.studby.ntnu.no <lulf@carrot.studby.ntnu.no>
2007-11-20 16:53:21 +0100

    - Add initial skeleton - Add a little readme text.

---
+++ src/mp3_vnops.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+static struct fuse_operations mp3_oper = {
+	.getattr	= mp3_getattr,
+	.readdir	= mp3_readdir,
+	.open		= mp3_open,
+	.read		= mp3_read,
+};
+
+
---
+++ src/mp3fs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+static struct fuse_module mp3_mod {
+	"MP3FS"
+
+};
+= { #name_, factory_, NULL, NULL, 0 };
+int
+main(int argc, char **argv)
+{
+
+	return (0);
+}
