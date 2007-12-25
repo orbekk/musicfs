057a40463753a55f48007d6eb21ed1afb0439208
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2007-12-25 01:06:12 +0100

    - Added *basic* option parsing. (So we can play with a source dir)

--- src/mp3fs.c
+++ src/mp3fs.c
@@ -1,16 +1,27 @@
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
 #define FUSE_USE_VERSION 26
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <fuse.h>
+
 #include "mp3_vnops.c"
+#include "mp3_opt_parse.c"
 
 /* static struct fuse_module mp3_mod { */
 /* 	.name = "MP3FS" */
 /* }; */
 
+/* Just for testing the argument parsing. TODO: fix something better */
+char *sourcedir = NULL;
+
 int
 main(int argc, char **argv)
 {
-	return fuse_main (argc, argv, &mp3_oper);
+	struct fuse_args args = FUSE_ARGS_INIT (argc, argv);
+
+	if (fuse_opt_parse(&args, NULL, NULL, mp3fs_opt_proc) != 0)
+		exit (1);
+
+	return (fuse_main (args.argc, args.argv, &mp3_oper, NULL));
 }
