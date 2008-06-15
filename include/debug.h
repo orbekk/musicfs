acbf5536ee7b226c9126428bfe67ae5a34509e6f
Kjetil Ørbekk <orbekk@pvv.ntnu.no>
2008-06-15 20:06:47 +0200

    - Made DEBUG write to file instead of stderr (which didn't work)

--- include/debug.h
+++ include/debug.h
@@ -1,10 +1,16 @@
 #ifndef _DEBUG_H_
 #define _DEBUG_H_
 
+
 #ifdef DEBUGGING
-#  define DEBUG(...) fprintf (stderr, __VA_ARGS__)
+#  include <stdio.h>
+#  define DEBUGPATH "debug.txt"
+#  define DEBUG(...) FILE *__f491 = fopen(DEBUGPATH, "a"); \
+                     fprintf (__f491, __VA_ARGS__);      \
+                     fclose(__f491);
 #else
 #  define DEBUG(...)
 #endif 
+
 
 #endif
